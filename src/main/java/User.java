cd912e89f0e450caa5322a4b8775b86bd57d3cc0
stevenlagoy <stevenlagoy@gmail.com>
2025-04-02 16:00:34 -0400

    update User interface

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,22 @@
 public interface User {
 
-    // data members
-    int userID;
-    String name;
-    String email;
-    EnumBody userType;
-    String username;
+    enum UserType {
+        Customer,
+        Administrator,
+        ExperienceProviderEmployee
+    }
+
+    int getUserID();
+    int setUserID(int ID);
+    String getName();
+    void setName(String name);
+    String getEmail();
+    void setEmail(String email);
+    UserType getUserType();
+    void setUserType(UserType userType);
+    String getUsername();
+    void setUsername(String username);
 
     // member methods
-    void choseAccount();
+    void closeAccount();
 }
