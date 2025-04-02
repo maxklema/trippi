a1c7f9ed0c4ca49e6df61e0dfdf260aa1d225058
stevenlagoy <stevenlagoy@gmail.com>
2025-04-02 15:51:22 -0400

    create Review class and ReviewManager

---
+++ src/main/java/ReviewManager.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ReviewManager {
+    
+    private static ArrayList<Review> reviews;
+    
+    public static ArrayList<Review> getReviews() {
+        return reviews;
+    }
+    public static void addReview(Review review) {
+        reviews.add(review);
+    }
+    public static void removeReview(Review review) {
+        reviews.remove(review);
+    }
+
+}
