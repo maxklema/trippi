b21ab77028f7c0beb3d692494f6d6d69f8106e8e
maxklema <maxklema@gmail.com>
2025-04-09 15:19:53 -0400

    Administrator access modifier changed to public

--- src/main/java/Administrator.java
+++ src/main/java/Administrator.java
@@ -1,34 +1,34 @@
 public class Administrator {
     private int adminID;
 
-    private int getAdminID() {
+    public int getAdminID() {
         return this.adminID;
     }
 
-    private void setAdminID(int newAdminID) {
+    public void setAdminID(int newAdminID) {
         this.adminID = newAdminID;
     }
 
-    private Boolean deleteUser(User user) {
+    public Boolean deleteUser(User user) {
         System.out.println("User " + user.getUserID() + " Deleted");
         return true;
     }
 
-    private Boolean deleteReview(Review review) {
+    public Boolean deleteReview(Review review) {
         System.out.println("Review " + review.getReviewID() + " deleted");
         return true;
     }
 
-    private Boolean ApproveExperienceProvider(ExperienceProvider experienceProvider) {
+    public Boolean ApproveExperienceProvider(ExperienceProvider experienceProvider) {
         System.out.println("Approved Experience Provider for " + experienceProvider.getName());
         return true;
     }
 
-    private void closeAccount(User user) {
+    public void closeAccount(User user) {
         System.out.println("Deleted account for " + user.getUserID());
     }
 
-    private ExperienceProvider addExperienceProvider(String name, String website, String corporateEmail) {
+    public ExperienceProvider addExperienceProvider(String name, String website, String corporateEmail) {
         return newExperienceProvider(name, website, corporateEmail);
     }
 }
